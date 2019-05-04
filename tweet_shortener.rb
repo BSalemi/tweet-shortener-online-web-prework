640cfeaa8e195f5ab193ff03af1d84641a3c9f67
Brian Salemi <briandsalemi@gmail.com>
2019-05-04 01:13:04 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,7 +33,7 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener(tweets)
-    tweets.collect do |tweet|
+    tweets.each do |tweet|
       if tweet.length > 140
         puts word_substituter(tweet)
       else
