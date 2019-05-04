754b48973035d282d35b4b35d0e355b194180699
Brian Salemi <briandsalemi@gmail.com>
2019-05-04 00:48:18 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+require 'pry'
+
+
+def dictionary
+ { "hello" => "hi",
+   "to" => "2",
+   "two"=> "2",
+   "too" => "2",
+   "for" => "4",
+   "four" => "4",
+   "be" => "b",
+   "you" => "u",
+   "at" => "@",
+   "and" => "&",
+ }
+ end
+
+ def word_substituter(string_of_tweets)
+   tweets = string_of_tweets.split(" ")
+   tweets.collect do |word|
+     if dictionary.keys.include?(word.downcase)
+        dictionary[word.downcase]
+     else
+        word
+      end
+    end.join(" ")
+  end
+
+def bulk_tweet_shortener(tweets)
+    tweets.collect do |tweet|
+      puts word_substituter(tweet)
+    end
+end
